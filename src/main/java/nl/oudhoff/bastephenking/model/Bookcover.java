099b585574895e3ce352c8214f4bd282f33cac17
Jacquelien84 <jacquelien.oudhoff@outlook.com>
2024-12-06 13:25:48 +0100

    Bookcover Model toegevoegd

---
+++ src/main/java/nl/oudhoff/bastephenking/model/Bookcover.java
@@ -0,0 +1,22 @@
+package nl.oudhoff.bastephenking.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "bookcovers")
+public class Bookcover {
+
+    @Id
+    private String fileName;
+
+    public Bookcover(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public Bookcover() {
+    }
+}
