fe9a05a49eef58d7ad0022e2fa962a790dfccc2c
Jacquelien84 <jacquelien.oudhoff@outlook.com>
2024-12-06 13:40:34 +0100

    book dto toegevoegd

--- src/main/java/nl/oudhoff/bastephenking/dto/mapper/BookMapper.java
+++ src/main/java/nl/oudhoff/bastephenking/dto/mapper/BookMapper.java
@@ -1,4 +1,36 @@
 package nl.oudhoff.bastephenking.dto.mapper;
 
+import nl.oudhoff.bastephenking.dto.input.BookInputDto;
+import nl.oudhoff.bastephenking.dto.output.BookOutputDto;
+import nl.oudhoff.bastephenking.model.Book;
+
 public class BookMapper {
+
+    public static Book fromInputDtoToModel (BookInputDto inputDto){
+        Book b = new Book();
+        b.setId(inputDto.getId());
+        b.setTitle(inputDto.getTitle());
+        b.setAuthor(inputDto.getAuthor());
+        b.setOriginalTitle(inputDto.getOriginalTitle());
+        b.setReleased(inputDto.getReleased());
+        b.setMovieAdaptation(inputDto.getMovieAdaptation());
+        b.setDescription(inputDto.getDescription());
+        return b;
+    }
+
+    public static BookOutputDto fromModelToOutputDto (Book book) {
+        BookOutputDto bookOutputDto = new BookOutputDto();
+        bookOutputDto.setId(book.getId());
+        bookOutputDto.setTitle(book.getTitle());
+        bookOutputDto.setAuthor(book.getAuthor());
+        bookOutputDto.setOriginalTitle(book.getOriginalTitle());
+        bookOutputDto.setReleased(book.getReleased());
+        bookOutputDto.setMovieAdaptation(book.getMovieAdaptation());
+        bookOutputDto.setDescription(book.getDescription());
+        if(book.getBookcover() != null){
+            bookOutputDto.setBookcover(book.getBookcover().getFileName());
+        }
+        return bookOutputDto;
+    }
 }
+
--- src/main/java/nl/oudhoff/bastephenking/dto/output/BookOutputDto.java
+++ src/main/java/nl/oudhoff/bastephenking/dto/output/BookOutputDto.java
@@ -1,4 +1,17 @@
 package nl.oudhoff.bastephenking.dto.output;
 
+import lombok.Data;
+
+@Data
 public class BookOutputDto {
+
+    public Object getId;
+    private Long id;
+    private String title;
+    private String author;
+    private String originalTitle;
+    private Long released;
+    private String movieAdaptation;
+    private String description;
+    private String bookcover;
 }
