d5c4fe20b6c6031abf59c850ffdb35734a476256
Jacquelien84 <jacquelien.oudhoff@outlook.com>
2024-12-06 13:00:51 +0100

    project voor eindopdracht opgestart

---
+++ src/main/java/nl/oudhoff/bastephenking/BaStephenKingApplication.java
@@ -0,0 +1,13 @@
+package nl.oudhoff.bastephenking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaStephenKingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaStephenKingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nl/oudhoff/bastephenking/BaStephenKingApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.oudhoff.bastephenking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaStephenKingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
