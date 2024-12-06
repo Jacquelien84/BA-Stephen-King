84af64621bcc0e895c13bfd69a4e04153a0e439e
Jacquelien84 <jacquelien.oudhoff@outlook.com>
2024-12-06 16:06:15 +0100

    login dto toegevoegd

--- src/main/java/nl/oudhoff/bastephenking/dto/dto/LoginRequestDto.java
+++ src/main/java/nl/oudhoff/bastephenking/dto/dto/LoginRequestDto.java
@@ -1,4 +1,17 @@
 package nl.oudhoff.bastephenking.dto.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
 public class LoginRequestDto {
+    @NotBlank(message = "Username is required")
+    private String username;
+    @NotBlank(message = "Password is required")
+    private String password;
+
+    public LoginRequestDto() {
+        this.username = username;
+        this.password = password;
+    }
 }
