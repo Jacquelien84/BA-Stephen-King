e2c850c9647220b27a964af9e3cdc166b9996914
Jacquelien84 <jacquelien.oudhoff@outlook.com>
2024-12-06 15:00:29 +0100

    userController toegevoegd

---
+++ src/main/java/nl/oudhoff/bastephenking/controller/UserController.java
@@ -0,0 +1,39 @@
+package nl.oudhoff.bastephenking.controller;
+
+
+import nl.oudhoff.bastephenking.dto.output.UserOutputDto;
+import nl.oudhoff.bastephenking.exception.ResourceNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UserOutputDto>> getAllUsers() {
+        return ResponseEntity.ok().body(userService.getAll());
+    }
+
+    @GetMapping("/{username}")
+    public ResponseEntity<UserOutputDto> getUser(@PathVariable("username") String username) {
+        UserOutputDto user = userService.get(username);
+        return ResponseEntity.ok().body(user);
+    }
+
+    @DeleteMapping("/delete/{username}")
+    public ResponseEntity<?> deleteUser(@PathVariable String username) throws ResourceNotFoundException {
+        userService.deleteUser(username);
+        return ResponseEntity.status(HttpStatus.OK).body("User with username " + username + " has been removed.");
+    }
+}
